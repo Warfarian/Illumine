6ef4a354463a6f6ae991e2815815beab3ae726c9
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-11-22 02:02:20 +0530

    setting up authentication for the users

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.ChoiceField(choices=["faculty", "student"], write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "role"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        role = validated_data.pop("role")
+        user = User.objects.create_user(**validated_data)
+        
+        if role == "faculty":
+            user.groups.add(Group.objects.get(name="Faculty"))
+        elif role == "student":
+            user.groups.add(Group.objects.get(name="Students"))
+        
+        user.save()
+        return user
